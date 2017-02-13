5d5567855d2e4bbcf8dfb9dbf93e79d6e6f845f3
Rebekah Shein <rmshein@gmail.com>
2017-02-12 20:35:02 -0500

    Update Pin model with paperclip image config

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+
+  # paperclip gem usage
+  has_attached_file :image, styles: { medium: "300X300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
